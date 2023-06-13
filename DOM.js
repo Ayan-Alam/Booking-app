066b5857abfb7b3dc21bc937ec77bc9bc16289f7
Ayan-Alam <ayanalam186@gmail.com>
2023-06-14 00:55:41 +0530

    "Delete data from Local Storage"

--- DOM.js
+++ DOM.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',function(){
     let form = document.getElementById('addform');
-    let itemList = document.getElementById('list-items')
-    form.addEventListener('submit', addItem);
+    let itemList = document.getElementById('list-items');
+    document.getElementById('submit-form').addEventListener('click', addItem);
      function addItem(e){
         let name = document.getElementById('name').value;
         let email = document.getElementById('email').value;
@@ -9,8 +9,19 @@ document.addEventListener('DOMContentLoaded',function(){
         e.preventDefault();
         var li = document.createElement('li');
         li.className = 'list-group-item';
-        li.appendChild(document.createTextNode('name : ' + name + ",  " + "Email-Adress: " + email +  ", Contact Details: " + phone));
+        li.appendChild(document.createTextNode(name + " - " +email+  " - " +phone));
+        var delet = document.createElement('button');
+        delet.id = 'submit2';
+        delet.type = 'submit';
+        delet.appendChild(document.createTextNode('Delete'));
+        li.appendChild(delet);
         itemList.appendChild(li);
+        document.getElementById('submit2').addEventListener('click', delItem);
+        function delItem(e){
+         e.preventDefault();
+         let li = e.target.parentElement;
+         itemList.removeChild(li);
+        }
         const my_obj = {
          name : name,
          email : email,
@@ -19,4 +30,5 @@ document.addEventListener('DOMContentLoaded',function(){
         let my_obj_string = JSON.stringify(my_obj);
         localStorage.setItem(email,my_obj_string);
      }
+     
 });
