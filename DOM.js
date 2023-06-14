cba8eaf8ab516eadb0a848300af62aa720084d4a
Ayan-Alam <ayanalam186@gmail.com>
2023-06-14 12:38:42 +0530

    "Edit Button"

--- DOM.js
+++ DOM.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',function(){
     let form = document.getElementById('addform');
     let itemList = document.getElementById('list-items');
-    document.getElementById('submit-form').addEventListener('click', addItem);
+    document.addEventListener('submit', addItem);
      function addItem(e){
         let name = document.getElementById('name').value;
         let email = document.getElementById('email').value;
@@ -11,22 +11,31 @@ document.addEventListener('DOMContentLoaded',function(){
         li.className = 'list-group-item';
         li.appendChild(document.createTextNode(name + " - " +email+  " - " +phone));
         var delet = document.createElement('button');
-        delet.id = 'submit2';
         delet.type = 'submit';
         delet.appendChild(document.createTextNode('Delete'));
         li.appendChild(delet);
         itemList.appendChild(li);
-        document.getElementById('submit2').addEventListener('click', delItem);
-        function delItem(e){
-         e.preventDefault();
-         let li = e.target.parentElement;
-         itemList.removeChild(li);
-        }
+        var edit = document.createElement('button');
+        edit.type = 'submit';
+        edit.appendChild(document.createTextNode('edit'));
+        li.appendChild(edit);
+        itemList.appendChild(li);
         const my_obj = {
          name : name,
          email : email,
          phone : phone,
-       }; 
+       };
+       delet.onclick = () =>{
+         localStorage.removeItem(my_obj.email);
+         itemList.removeChild(li);
+        }
+        edit.onclick = () =>{
+           localStorage.removeItem(my_obj.email);
+           document.getElementById('name').value = my_obj.name;
+           document.getElementById('email').value = my_obj.email;
+           document.getElementById('phone').value = my_obj.phone;
+           itemList.removeChild(li);
+         }    
         let my_obj_string = JSON.stringify(my_obj);
         localStorage.setItem(email,my_obj_string);
      }
